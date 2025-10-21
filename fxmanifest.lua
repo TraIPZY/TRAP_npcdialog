91a9b439f788f945381bc3bb5aa7123f65bb8fd3
Trap <148614470+TraIPZY@users.noreply.github.com>
2025-10-21 16:18:22 -0400

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ game 'gta5'
 
 author 'TraPZY'
 description 'TRAP NPC Dialogue - NPC interaction avec paiement ox_inventory'
-version '1.2.0'
+version '1.2'
 
 shared_scripts {
     '@ox_lib/init.lua',
@@ -23,3 +23,4 @@ dependencies {
     'ox_target',
     'ox_inventory'
 }
+
